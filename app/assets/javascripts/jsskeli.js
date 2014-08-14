a6381f957113240131e62d4e74713ca416376c4f
Reid Covington <reid@shopcube.com>
2014-08-14 11:51:35 -0400

    refactor and rename cube view rotations

--- app/assets/javascripts/jsskeli.js
+++ app/assets/javascripts/jsskeli.js
@@ -145,7 +145,7 @@ $(document).ready(function(){
         },
         receiveTurnDirection: function(direction){
             this.cubeModel.updateSideFacing(direction);
-            this.cubeView.rotate(direction);
+            this.cubeView.rotateCube(direction);
         },
         checkOldFace: function(){
             this.delegate.checkForTotalMatch();
@@ -212,14 +212,52 @@ $(document).ready(function(){
         this.xAngle = 0;
         this.yAngle = 0;
         this.matrix = [
-                //   f, bk, l, t, r, bt
+                     //   f, bk, l, t, r, bt
       /* side 1 */  [ 1, 0, 0, 0, 0, 0 ],
       /* side 2 */  [ 0, 1, 0, 0, 0, 0 ],
       /* side 3 */  [ 0, 0, 1, 0, 0, 0 ],
       /* side 4 */  [ 0, 0, 0, 1, 0, 0 ],
       /* side 5 */  [ 0, 0, 0, 0, 1, 0 ],
       /* side 6 */  [ 0, 0, 0, 0, 0, 1 ]
-];
+        ];
+        this.rotations = {
+            left: [
+                //   f, bk, l, t, r, bt
+      /* side 1 */  [ 0, 0, 1, 0, 0, 0 ],
+      /* side 2 */  [ 0, 0, 0, 0, 1, 0 ],
+      /* side 3 */  [ 0, 1, 0, 0, 0, 0 ],
+      /* side 4 */  [ 0, 0, 0, 1, 0, 0 ],
+      /* side 5 */  [ 1, 0, 0, 0, 0, 0 ],
+      /* side 6 */  [ 0, 0, 0, 0, 0, 1 ]
+        ],
+        right: [
+                //   f, bk, l, t, r, bt
+      /* side 1 */  [ 0, 0, 0, 0, 1, 0 ],
+      /* side 2 */  [ 0, 0, 1, 0, 0, 0 ],
+      /* side 3 */  [ 1, 0, 0, 0, 0, 0 ],
+      /* side 4 */  [ 0, 0, 0, 1, 0, 0 ],
+      /* side 5 */  [ 0, 1, 0, 0, 0, 0 ],
+      /* side 6 */  [ 0, 0, 0, 0, 0, 1 ]
+        ],
+         up: [
+                //   f, bk, l, t, r, bt
+      /* side 1 */  [ 0, 0, 0, 1, 0, 0 ],
+      /* side 2 */  [ 0, 0, 0, 0, 0, 1 ],
+      /* side 3 */  [ 0, 0, 1, 0, 0, 0 ],
+      /* side 4 */  [ 0, 1, 0, 0, 0, 0 ],
+      /* side 5 */  [ 0, 0, 0, 0, 1, 0 ],
+      /* side 6 */  [ 1, 0, 0, 0, 0, 0 ]
+        ],
+        down: [
+                //   f, bk, l, t, r, bt
+      /* side 1 */  [ 0, 0, 0, 0, 0, 1 ],
+      /* side 2 */  [ 0, 0, 0, 1, 0, 0 ],
+      /* side 3 */  [ 0, 0, 1, 0, 0, 0 ],
+      /* side 4 */  [ 1, 0, 0, 0, 0, 0 ],
+      /* side 5 */  [ 0, 0, 0, 0, 1, 0 ],
+      /* side 6 */  [ 0, 1, 0, 0, 0, 0 ]
+        ]
+    };
         this.assignFaces();
         this._setupButtonClickListeners();
     };
@@ -227,7 +265,6 @@ $(document).ready(function(){
         _setupButtonClickListeners: function(){
             var self = this;
             $('[data-direction]').click(function(e){
-                console.log('clicked');
                 self.delegate.receiveTurnDirection($(this).data('direction'));
             })
         },
@@ -247,18 +284,31 @@ $(document).ready(function(){
         hideButtons: function(number){
             $(".cubeface"+number+"buttons").hide();
         },
-        rotate: function(direction){
+        rotateCube: function(direction){
             // var directions = direction.split(" ");
             // this.xAngle += parseInt(directions[0]);
             // this.yAngle += parseInt(directions[1]);
             // $('#cube').css("-webkitTransform", "rotateX("+this.xAngle+"deg) rotateY("+this.yAngle+"deg)");
             // debugger
-            this.matrix = rotateCube(this.matrix, rotations[direction]);
+            this.matrix = this.createRotatedMatrix(this.matrix, this.rotations[direction]);
             this.assignFaces();
+        },
+        createRotatedMatrix: function(current, direction){
+            var newMatrix = [];
+            for(var row = 0; row < current.length; row++){
+                newMatrix[row] = [];
+                for(var column = 0; column < direction[0].length; column++){
+                    var sum = 0;
+                    for(var i = 0; i < current[row].length; i++){
+                        sum += current[row][i] * direction[i][column];
+                    }
+                    newMatrix[row].push(sum);
+                }
+            }
+            return newMatrix;
         },
         assignFaces: function(){
             var faces = ["front", "back", "left", "top", "right", "bottom"]
-
             for(var i = 0; i < this.matrix.length; i++){
                 var direction = faces[this.matrix[i].indexOf(1)];
                 $('[data-side=' + i + ']').attr("class", "face " + direction)
@@ -267,60 +317,6 @@ $(document).ready(function(){
     };
 
 
-var rotations = {
-    left: [
-                //   f, bk, l, t, r, bt
-      /* side 1 */  [ 0, 0, 1, 0, 0, 0 ],
-      /* side 2 */  [ 0, 0, 0, 0, 1, 0 ],
-      /* side 3 */  [ 0, 1, 0, 0, 0, 0 ],
-      /* side 4 */  [ 0, 0, 0, 1, 0, 0 ],
-      /* side 5 */  [ 1, 0, 0, 0, 0, 0 ],
-      /* side 6 */  [ 0, 0, 0, 0, 0, 1 ]
-],
 
-    right: [
-                //   f, bk, l, t, r, bt
-      /* side 1 */  [ 0, 0, 0, 0, 1, 0 ],
-      /* side 2 */  [ 0, 0, 1, 0, 0, 0 ],
-      /* side 3 */  [ 1, 0, 0, 0, 0, 0 ],
-      /* side 4 */  [ 0, 0, 0, 1, 0, 0 ],
-      /* side 5 */  [ 0, 1, 0, 0, 0, 0 ],
-      /* side 6 */  [ 0, 0, 0, 0, 0, 1 ]
-],
 
-    up: [
-                //   f, bk, l, t, r, bt
-      /* side 1 */  [ 0, 0, 0, 1, 0, 0 ],
-      /* side 2 */  [ 0, 0, 0, 0, 0, 1 ],
-      /* side 3 */  [ 0, 0, 1, 0, 0, 0 ],
-      /* side 4 */  [ 0, 1, 0, 0, 0, 0 ],
-      /* side 5 */  [ 0, 0, 0, 0, 1, 0 ],
-      /* side 6 */  [ 1, 0, 0, 0, 0, 0 ]
-],
-
-    down: [
-                //   f, bk, l, t, r, bt
-      /* side 1 */  [ 0, 0, 0, 0, 0, 1 ],
-      /* side 2 */  [ 0, 0, 0, 1, 0, 0 ],
-      /* side 3 */  [ 0, 0, 1, 0, 0, 0 ],
-      /* side 4 */  [ 1, 0, 0, 0, 0, 0 ],
-      /* side 5 */  [ 0, 0, 0, 0, 1, 0 ],
-      /* side 6 */  [ 0, 1, 0, 0, 0, 0 ]
-]
-}
-
-function rotateCube(current, direction){
-    var result = [];
-    for(var row = 0; row < current.length; row++){
-        result[row] = [];
-        for(var column = 0; column < direction[0].length; column++){
-            var sum = 0;
-            for(var i = 0; i < current[row].length; i++){
-                sum += current[row][i] * direction[i][column];
-            }
-            result[row].push(sum);
-        }
-    }
-    return result;
-}
 
